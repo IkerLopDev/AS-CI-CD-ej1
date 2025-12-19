dcc7c61659ec602d98ab09e6a72387ff010a400e
Iker LA <76450243+IkerLopDev@users.noreply.github.com>
2025-12-19 14:36:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -12,5 +12,5 @@ def test_removebalance():
 
 def test_setbalance():
     obj = Wallet(0)
-    obj.set_balance(40)
-    assert obj.get_balance() == 40
+    obj.set_balance(50)
+    assert obj.get_balance() == 50
